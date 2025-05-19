e7c2bce31e6b123700a3e9ea7b7dd1e4e6e747d2
Pablo <pablomaia533@gmail.com>
2025-05-19 10:32:03 -0300

    Implement Product Registration Component

--- src/app/features/produto/produto.component.ts
+++ src/app/features/produto/produto.component.ts
@@ -1,12 +1,97 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ProdutoTipo } from '../../shared/enums/produtotipo';
+import { Categoria } from '../../core/models/categoria';
+import { Fabricante } from '../../core/models/fabricante';
+import { ProdutoService } from '../../shared/services/produto.service';
 
 @Component({
   selector: 'app-produto',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './produto.component.html',
-  styleUrl: './produto.component.css'
+  styleUrls: ['./produto.component.css']
 })
-export class ProdutoComponent {
+export class ProdutoComponent implements OnInit {
+  productForm!: FormGroup;
+  isSubmitting = false;
+  ProdutoTipo = ProdutoTipo;
+  categorias: Categoria[] = [];
+  fabricantes: Fabricante[] = [];
 
+  constructor(
+    private fb: FormBuilder,
+    private produtoService: ProdutoService
+  ) {}
+
+  ngOnInit(): void {
+    this.initForm();
+    this.loadCategorias();
+    this.loadFabricantes();
+  }
+
+  initForm(): void {
+    this.productForm = this.fb.group({
+      produto_nome: ['', Validators.required],
+      produto_identificacao: ['', Validators.required],
+      produto_tipo: ['', Validators.required],
+      categoria_id: ['', Validators.required],
+      fabricante_id: ['', Validators.required],
+      produto_descricao: ['', Validators.required],
+      produto_status: [1] // Default active status
+    });
+  }
+
+  loadCategorias(): void {
+    // TODO: Implement categoria service call
+    this.categorias = [];
+  }
+
+  loadFabricantes(): void {
+    // TODO: Implement fabricante service call
+    this.fabricantes = [];
+  }
+
+  onSubmit(): void {
+    if (this.productForm.invalid) {
+      Object.keys(this.productForm.controls).forEach(key => {
+        const control = this.productForm.get(key);
+        if (control?.invalid) {
+          control.markAsTouched();
+        }
+      });
+      return;
+    }
+
+    this.isSubmitting = true;
+    this.produtoService.createProduto(this.productForm.value).subscribe({
+      next: () => {
+        this.onReset();
+        // TODO: Show success message
+      },
+      error: (error) => {
+        console.error('Error creating product:', error);
+        // TODO: Show error message
+      },
+      complete: () => {
+        this.isSubmitting = false;
+      }
+    });
+  }
+
+  onReset(): void {
+    this.productForm.reset();
+    this.initForm();
+  }
+
+  hasError(controlName: string, errorName: string): boolean {
+    const control = this.productForm.get(controlName);
+    return !!control && control.touched && control.hasError(errorName);
+  }
+
+  isFieldInvalid(controlName: string): boolean {
+    const control = this.productForm.get(controlName);
+    return !!control && control.touched && control.invalid;
+  }
 }
--- src/app/shared/services/produto.service.ts
+++ src/app/shared/services/produto.service.ts
@@ -1,19 +1,36 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../enviroment';
-
-const API = environment.microserviceProduto
-
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type': 'application/json',
-  })
-};
+import { Observable } from 'rxjs';
+import { Produto } from '../../core/models/produto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProdutoService {
+  private readonly baseUrl: string;
 
-  constructor(private http : HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.baseUrl = `${environment.microserviceProduto}/produtos`;
+  }
+
+  createProduto(produto: Produto): Observable<Produto> {
+    return this.http.post<Produto>(this.baseUrl, produto);
+  }
+
+  getProdutos(): Observable<Produto[]> {
+    return this.http.get<Produto[]>(this.baseUrl);
+  }
+
+  getProdutoById(id: number): Observable<Produto> {
+    return this.http.get<Produto>(`${this.baseUrl}/${id}`);
+  }
+
+  updateProduto(id: number, produto: Produto): Observable<Produto> {
+    return this.http.put<Produto>(`${this.baseUrl}/${id}`, produto);
+  }
+
+  deleteProduto(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${id}`);
+  }
 }
