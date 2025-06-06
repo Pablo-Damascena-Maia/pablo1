4ccda303dfdd46f42e096e96dfd4dcacd3536b30
Pablo <pablomaia533@gmail.com>
2025-06-06 08:23:35 -0300

    Fix CORS issue with proxy configuration

--- src/enviroment.ts
+++ src/enviroment.ts
@@ -4,6 +4,6 @@ export const environment = {
   microserviceUsuario:    'http://academico3.rj.senac.br/usuario',
   microserviceCarrinho:   'http://academico3.rj.senac.br/carrinho',
   microservicePagamento:  'http://academico3.rj.senac.br/pagamento',
-  microserviceProduto:    'http://academico3.rj.senac.br/produto',
+  microserviceProduto:    '/api/produto-service',
   microserviceEstoque:    'http://academico3.rj.senac.br/estoque',
 };
